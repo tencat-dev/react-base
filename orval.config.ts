b17c1c8eb3a1685986919b19e74c5ef70a1cb235
mizhm <minhnt141204@gmail.com>
2025-11-14 09:44:09 +0700

    feat: update biome + knip

---
+++ orval.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  api: {
+    output: {
+      target: "src/services/apis/gen/queries.ts",
+      prettier: true,
+      clean: true,
+      client: "react-query",
+      override: {
+        useTypeOverInterfaces: true,
+        query: {
+          useInfinite: true,
+          useQuery: true,
+        },
+        mutator: {
+          path: "src/services/apis/axios-client.ts",
+          name: "orvalClient",
+        },
+      },
+    },
+    input: {
+      validation: false,
+      target: "../.open-api/open-api.json",
+    },
+  },
+});

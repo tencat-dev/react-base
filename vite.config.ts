e2360c245b2ed3bf40eab7e02e5ff352930203b7
mizhm <minhnt141204@gmail.com>
2025-11-16 10:50:35 +0700

    feat: update vite analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import {defineConfig} from 'vite'
-import {devtools} from '@tanstack/devtools-vite'
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-import {tanstackRouter} from '@tanstack/router-plugin/vite'
-import {fileURLToPath, URL} from 'node:url'
-import compression from "vite-plugin-compression";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { fileURLToPath, URL } from 'node:url'
+import compression from 'vite-plugin-compression'
+
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    process.env.NODE_ENV !== "production" && devtools(),
+    process.env.NODE_ENV !== 'production' && devtools(),
 
     tanstackRouter({
       target: 'react',
@@ -20,9 +22,12 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
 
-    process.env.NODE_ENV === "production" && compression({
-      algorithm: "brotliCompress"
-    }),
+    process.env.NODE_ENV === 'production' &&
+      compression({
+        algorithm: 'brotliCompress',
+      }),
+
+    analyzer(),
   ],
   resolve: {
     alias: {
@@ -30,28 +35,28 @@ export default defineConfig({
     },
   },
   build: {
-    minify: "esbuild",
-    target: "es2020",
-    sourcemap: process.env.NODE_ENV !== "production",
-    cssCodeSplit: process.env.NODE_ENV === "production",
-    cssMinify: process.env.NODE_ENV === "production",
+    minify: 'esbuild',
+    target: 'es2020',
+    sourcemap: process.env.NODE_ENV !== 'production',
+    cssCodeSplit: process.env.NODE_ENV === 'production',
+    cssMinify: process.env.NODE_ENV === 'production',
     assetsInlineLimit: 2048, // 2kb
 
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ["react", "react-dom"],
-          "react-router": ["@tanstack/react-router"],
-          "react-query": ["@tanstack/react-query"],
-          "react-table": ["@tanstack/react-table"],
-          "react-form": ["@tanstack/react-form"],
-          zod: ["zod"],
-          icons: ["lucide-react"],
+          react: ['react', 'react-dom'],
+          'react-router': ['@tanstack/react-router'],
+          'react-query': ['@tanstack/react-query'],
+          'react-table': ['@tanstack/react-table'],
+          'react-form': ['@tanstack/react-form'],
+          zod: ['zod'],
+          icons: ['lucide-react'],
         },
       },
     },
   },
   esbuild: {
-    drop: ["console", "debugger"],
+    drop: ['console', 'debugger'],
   },
 })

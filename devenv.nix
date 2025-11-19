c04d9c02abe04914a01108a82dcbafb256d3ed79
mizhm <minhnt141204@gmail.com>
2025-11-19 22:44:42 +0700

    feat: pin version of node to 24.11.1 and bun to 1.3.2

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,22 @@
-{pkgs, ...}: {
+{inputs, pkgs, ...}: {
   env.BIOME_BINARY = "@biomejs/cli-linux-x64-musl/biome";
+  overlays = [
+      (final: prev: {
+        nodejs-slim = (import inputs.nodeslim-24 {
+          system = prev.stdenv.system;
+        }).nodejs-slim_24;
+        bun = (import inputs.bun-132 {
+          system = prev.stdenv.system;
+        }).bun;
+      })
+    ];
+
   packages = [pkgs.git];
 
   languages.javascript = {
     enable = true;
     bun.enable = true;
   };
-
-  languages.typescript.enable = true;
-
   # https://devenv.sh/git-hooks/
   # git-hooks.hooks.shellcheck.enable = true;
 }
